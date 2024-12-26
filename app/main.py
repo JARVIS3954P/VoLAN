1b5e2e6adc9d5fd018a3065124af85a658d0edf2
lol-boi <amanwastakenwastaken1@gmail.com>
2024-12-26 05:55:21 +0530

    Worked on main.py(TCP, PTP)

--- app/main.py
+++ app/main.py
@@ -1,43 +1,38 @@
-import socket
-import sys
-from scapy.all import ARP, Ether, srp
+'''
+Right now program just setups a TCP connection between two devices (PTP connection) connected on lan in our case we talk to our own device,
+but it will also list the router(moblie hotspot)
+Usage is as follows....
+1. Run the script and select the server option first
+2. Once the server starts listening run the Client
+3. Client lists your IP and all the other avalable IP on the network
+4. Choose your device's IP and type the message for Server
+5. For closing the connection type "close" and send to server
+
+This script will also work on two different devices in the same local network if they both run this same python script
+'''
+
 import logging
 import os
+import core.networking as nt
 
-# Ensure the logs/ folder exists
 os.makedirs("logs", exist_ok=True)
-
-# Configure logging
 logging.basicConfig(
-    level=logging.DEBUG,  # Set the logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
-    format="%(asctime)s - %(levelname)s - %(message)s",  # Log format
+    level=logging.DEBUG,  
+    format="%(asctime)s - %(levelname)s - %(message)s", 
     handlers=[
-        logging.FileHandler("logs/app.log"),  # Log to a file
-        logging.StreamHandler()  # Log to the console
+        logging.FileHandler("logs/app.log"),
+        logging.StreamHandler()  
     ]
 )
-def get_local_ip():
-    """Get the local IP address of the machine."""
-    try:
-        hostname = socket.gethostname()
-        local_ip = socket.gethostbyname(hostname)
-        logging.info(f"Hostname: {hostname}")
-        logging.info(f"Local IP: {local_ip}")
-        return local_ip
-    except Exception as e:
-        logging.error(f"Error fetching local IP: {e}")
-        sys.exit(1)
 
 
-def find_devices_on_lan():
-    local_ip = get_local_ip()
 
-
-# Example usage
 if __name__ == "__main__":
-    find_devices_on_lan();
-    logging.debug("This is a debug message.")
-    logging.info("This is an info message.")
-    logging.warning("This is a warning message.")
-    logging.error("This is an error message.")
-    logging.critical("This is a critical message.")
+    print("READ THE PYTHON FILE FIRST BEFORE USING!!!")
+    option = int(input("1. Sender (Client)\n2. Reciver (Server)\nEnter you choice: "))
+    if option == 1:
+        nt.run_client()  
+    elif option == 2:
+        nt.run_server()
+    else:
+        print("INVALID OPTION TERMINATING..")
