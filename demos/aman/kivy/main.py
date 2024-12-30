7e68a5a306093b11f3d34f0a6a32910ae1d399b0
lol-boi <amanwastakenwastaken1@gmail.com>
2024-12-30 23:36:38 +0530

    Learining GUI with kivy 1.0

---
+++ demos/aman/kivy/main.py
@@ -0,0 +1,18 @@
+import kivy
+from kivy.app import App
+from kivy.uix.behaviors.button import ButtonBehavior
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+
+
+class box_layout_example(BoxLayout):
+    pass
+class first_widget(Widget):
+    pass
+
+class firstApp(App):
+    pass
+
+app = firstApp()
+app.run()
