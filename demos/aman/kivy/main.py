5b480dabe3da9cd99ebe998b6290cf7c40b45b31
lol-boi <amanwastakenwastaken1@gmail.com>
2024-12-31 23:52:08 +0530

    Kivy GUI Demo 2.0

--- demos/aman/kivy/main.py
+++ demos/aman/kivy/main.py
@@ -4,10 +4,23 @@ from kivy.uix.behaviors.button import ButtonBehavior
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
 
 
-class box_layout_example(BoxLayout):
-    pass
+class Stack_layout_example(StackLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        b = Button(text = "Z", size_hint =(.2,.2))
+        self.add_widget(b)
+
+#class anchor_layout_example(AnchorLayout):
+#    pass
+
+#class Box_layout_example(BoxLayout):
+#    pass
+
 class first_widget(Widget):
     pass
 
